374bb3d3376cfacd3745eb32c07ee813f0a3df6e
Oguz <yilmazoguz@outlook.com>
2022-01-31 14:12:01 +0300

    Import user and give its props under h1 within Home.jsx

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import {user} from '../data/user';
 
 function Links() {
   return (
     <div>
         <h3>Links</h3>
+        <a href={props.Links.github}></a>
+        <a href={props.Links.linkedin}></a>
     </div>
   );
 }
