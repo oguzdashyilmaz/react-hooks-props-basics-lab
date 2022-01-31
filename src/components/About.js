25cf733b6e9a0584e56e9a46c0899d16a53e0996
Oguz <yilmazoguz@outlook.com>
2022-01-31 16:54:32 +0300

    commit tree bc0aa13bde3c3797df5faf291c6e622fc2764344

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,15 @@
 import React from "react";
 import user from "../data/user";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>{user.bio}</p>
+      {(props.hasOwnProperty("bio") || props.bio !== "") ? <p>{props.bio}</p> : ""}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
