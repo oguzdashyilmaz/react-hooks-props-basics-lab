c1f004eae7d9ed484bd6c085f2cf9416f6affd49
Oguz <yilmazoguz@outlook.com>
2022-01-31 13:51:38 +0300

    Create Links.jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,11 @@
 import React from "react";
+import user from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{user.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
     </div>
